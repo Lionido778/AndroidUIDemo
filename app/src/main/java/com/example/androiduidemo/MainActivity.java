0af474ca89b68c283d377f183c9529482f9a1302
Lionido <Lionido@gmail.com>
2019-10-30 19:20:08 +0800

    second commit

--- app/src/main/java/com/example/androiduidemo/MainActivity.java
+++ app/src/main/java/com/example/androiduidemo/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.androiduidemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.caculator_layout);
+
+    }
+
+    public void oneOnclick(View view) {
+        if (view instanceof TextView) {
+            String text = ((TextView) view).getText().toString();
+            Log.d(TAG,"控件内容 : "+ text);
+        }
+    }
+
+    public void oneOrangleOnclick(View view) {
+        if (view instanceof TextView) {
+            String text = ((TextView) view).getText().toString();
+            Log.d(TAG,"控件内容 : "+ text);
+        }
     }
 }
